fb4ba3c3fe09a0b5222bb7cd9a4989e82f727132
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-06-19 03:16:24 +0700

    feat: add API_BASE_URL, API_VERSION env

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,9 +7,17 @@ if (!env.VERIFICATION_URL) {
 	throw new ReferenceError('VERIFICATION_URL must be provided');
 }
 
+if (!env.API_BASE_URL) {
+	throw new ReferenceError('API_BASE_URL must be provided');
+}
+
+if (!env.API_VERSION) {
+	throw new ReferenceError('API_VERSION must be provided');
+}
+
 const apiClient = new HttpApiClient({
-	baseUrl: 'http://127.0.0.1:5051/api',
-	version: 'v1'
+	baseUrl: env.API_BASE_URL,
+	version: env.API_VERSION
 });
 
 const ApiLive = Layer.succeed(ApiClientTag, apiClient);
