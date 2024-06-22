754edc32c496f79e219e0c9a61f93d764f58334f
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-06-22 18:46:52 +0700

    style(sidebar): sm font size on side bar

--- src/routes/(sidebar)/+layout.svelte
+++ src/routes/(sidebar)/+layout.svelte
@@ -7,8 +7,8 @@
 </script>
 
 <div class="flex w-full h-full min-w-screen min-h-screen bg-base-2 p-2 pl-0">
-	<aside class="items-stretch w-60 px-4 py-2 space-y-4">
-		<div class="select-none flex items-center gap-2 font-bold text-base-fg-1">
+	<aside class="items-stretch w-60 px-4 py-2 space-y-4 text-sm">
+		<div class="select-none flex items-center gap-2 font-bold text-base-fg-1 text-base">
 			<Logo class="h-8 w-auto" />
 			<p>plan</p>
 		</div>
@@ -30,7 +30,7 @@
 			]}
 		/>
 	</aside>
-	<main class="bg-base-1 grow rounded-lg border border-base-border shadow-sm p-16">
+	<main class="bg-base-1 grow rounded-md border border-base-border shadow-sm p-16">
 		<div class="max-w-6xl mx-auto">
 			{@render children()}
 		</div>
