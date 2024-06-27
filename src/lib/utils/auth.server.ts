42c093faf89bd76f44ef2683291caddaedc809d6
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-06-28 06:50:17 +0700

    feat(utils): add refresh token utils function

--- src/lib/utils/auth.server.ts
+++ src/lib/utils/auth.server.ts
@@ -11,25 +11,35 @@ export const authenticateOrRefresh = (cookies: Cookies) =>
 				return yield* Effect.fail<void>(void 0);
 			}
 
-			const apiClient = yield* ApiClientTag;
-			const response = yield* apiClient.post('tokens/refresh', { body: { refreshToken } });
-			if (response.ok) {
-				const result = yield* Effect.tryPromise(() =>
-					response.json<{
-						accessToken: string;
-						accessTokenMaxAge: number;
-					}>()
-				);
-				cookies.set('access_token', result.accessToken, {
-					path: '/',
-					httpOnly: true,
-					secure: true,
-					sameSite: 'strict',
-					maxAge: result.accessTokenMaxAge
-				});
-				return yield* Effect.void;
-			}
+			return yield* refresh(cookies);
+		}
+		return yield* Effect.succeed({ accessToken });
+	});
+
+export const refresh = (cookies: Cookies) =>
+	Effect.gen(function* () {
+		const refreshToken = cookies.get('refresh_token');
+		if (!refreshToken) {
 			return yield* Effect.fail<void>(void 0);
 		}
-		return yield* Effect.void;
+
+		const apiClient = yield* ApiClientTag;
+		const response = yield* apiClient.post('tokens/refresh', { body: { refreshToken } });
+		if (response.ok) {
+			const result = yield* Effect.tryPromise(() =>
+				response.json<{
+					accessToken: string;
+					accessTokenMaxAge: number;
+				}>()
+			);
+			cookies.set('access_token', result.accessToken, {
+				path: '/',
+				httpOnly: true,
+				secure: true,
+				sameSite: 'strict',
+				maxAge: result.accessTokenMaxAge
+			});
+			return yield* Effect.succeed({ accessToken: result.accessToken });
+		}
+		return yield* Effect.fail<void>(void 0);
 	});
