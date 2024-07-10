b93f3dac9921b0622434d05f0ab4e69dd18502fc
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-07-10 20:15:01 +0700

    style(Breadcrumb): add transition

--- src/lib/components/Breadcrumb.svelte
+++ src/lib/components/Breadcrumb.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import clsx from 'clsx';
+	import { cubicInOut } from 'svelte/easing';
 	import type { HTMLOlAttributes } from 'svelte/elements';
+	import { fly } from 'svelte/transition';
 	import Icon from './Icon.svelte';
 	import Link from './Link.svelte';
-	import clsx from 'clsx';
 
 	type Props = HTMLOlAttributes;
 
@@ -12,9 +14,16 @@
 
 {#if $page.data.routes}
 	<ol {...props} class={clsx('flex gap-2 text-sm', props.class)}>
-		{#each $page.data.routes.filter((x) => x.breadcrumb) as { meta: { title, href } }, i}
+		{#each $page.data.routes.filter((x) => x.breadcrumb) as { meta: { title, href } }, i (href)}
+			{@const notFirst = i !== 0}
 			{@const isLast = i === $page.data.routes.length - 1}
-			<li class="flex items-center gap-2 text-base-fg-3">
+			<li
+				class="flex items-center gap-2 text-base-fg-3"
+				transition:fly={{ duration: 200, x: '-0.2rem', easing: cubicInOut }}
+			>
+				{#if notFirst}
+					<Icon name="chevron-right" class={isLast ? 'text-base-fg-1' : undefined} />
+				{/if}
 				{#if isLast}
 					<span class="text-base-fg-1 font-bold">
 						{title}
@@ -23,9 +32,6 @@
 					<Link {href}>
 						{title}
 					</Link>
-				{/if}
-				{#if !isLast}
-					<Icon name="chevron-right" />
 				{/if}
 			</li>
 		{/each}
