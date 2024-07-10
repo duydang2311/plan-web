6958d737cb138bee2de1b324b6142b151bd1c5cb
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-07-10 19:53:09 +0700

    feat(layout): fetch workspace data

--- src/routes/(auth)/[path]/+layout.server.ts
+++ src/routes/(auth)/[path]/+layout.server.ts
@@ -3,19 +3,38 @@ import type { LayoutServerLoad } from './$types';
 import { Effect, Exit } from 'effect';
 import { error } from '@sveltejs/kit';
 
+const pathIdMap = new Map<string, string>();
+
 export const load: LayoutServerLoad = async ({ params, locals: { runtime } }) => {
 	const exit = await runtime.runPromiseExit(
 		Effect.gen(function* () {
 			const apiClient = yield* ApiClientTag;
-			const response = yield* apiClient.head('workspaces', {
-				query: { path: params.path }
-			});
+			const id = pathIdMap.get(params.path);
+			let response: Response;
+			if (id) {
+				response = yield* apiClient.get(`workspaces/${id}`, {
+					query: { select: 'new { Id, Name }' }
+				});
+			} else {
+				response = yield* apiClient.get(`workspaces/path/${params.path}`, {
+					query: { select: 'new { Id, Name }' }
+				});
+			}
 			if (!response.ok) {
 				return yield* Effect.fail<void>(void 0);
 			}
+
+			const json = yield* Effect.promise<{ id: string; name: string }>(() => response.json());
+			if (!id) {
+				pathIdMap.set(params.path, json.id);
+			}
+			return json;
 		})
 	);
 	if (Exit.isFailure(exit)) {
-		error(404, { message: 'Workspace does not exist', code: 'workspace_not_found' });
+		return error(404, { message: 'Workspace does not exist', code: 'workspace_not_found' });
 	}
+	return {
+		workspace: exit.value
+	};
 };
