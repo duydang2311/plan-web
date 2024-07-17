34ec0239e1451ca8973f513dc7daf5ef5d8993e3
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-07-17 15:49:35 +0700

    feat: add TextArea component

---
+++ src/lib/components/TextArea.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import clsx from 'clsx';
+	import type { HTMLTextareaAttributes } from 'svelte/elements';
+
+	type Props = Exclude<HTMLTextareaAttributes, 'children'>;
+
+	let { value = $bindable(), ...props }: Props = $props();
+	let element: HTMLTextAreaElement;
+
+	$effect(() => {
+		if (document.activeElement !== element && props['aria-invalid']) {
+			element.focus();
+		}
+	});
+</script>
+
+<textarea bind:this={element} {...props} bind:value class={clsx('c-input', props.class)}></textarea>
