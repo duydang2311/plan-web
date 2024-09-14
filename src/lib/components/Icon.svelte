18251877e6746b91842faa888810ff99e13405d1
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-09-15 02:17:40 +0700

    feat: lazy icon

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -1,103 +1,82 @@
 <script lang="ts" module>
-    import Home from '~icons/ph/house-bold';
-    import HomeSolid from '~icons/ph/house-fill';
-    import UserGroup from '~icons/heroicons/user-group-solid';
-    import UserGroupOutline from '~icons/heroicons/user-group';
-    import Plus from '~icons/heroicons/plus-20-solid';
-    import XMark from '~icons/heroicons/x-mark-20-solid';
-    import ChevronLeft from '~icons/heroicons/chevron-left-20-solid';
-    import ChevronRight from '~icons/heroicons/chevron-right-20-solid';
-    import ArrowUp from '~icons/heroicons/arrow-up-20-solid';
-    import ArrowDown from '~icons/heroicons/arrow-down-20-solid';
-    import ArrowsUpDown from '~icons/heroicons/arrows-up-down-20-solid';
-    import H1 from '~icons/lucide/heading-1';
-    import H2 from '~icons/lucide/heading-2';
-    import H3 from '~icons/lucide/heading-3';
-    import H4 from '~icons/lucide/heading-4';
-    import H5 from '~icons/lucide/heading-5';
-    import H6 from '~icons/lucide/heading-6';
-    import Bold from '~icons/lucide/bold';
-    import Italic from '~icons/lucide/italic';
-    import StrikeThrough from '~icons/lucide/strikethrough';
-    import Underline from '~icons/lucide/underline';
-    import Quotes from '~icons/lucide/quote';
-    import Files from '~icons/ph/files-fill';
-    import FilesOutline from '~icons/ph/files';
-    import SettingsOutline from '~icons/heroicons/cog-6-tooth';
-    import Settings from '~icons/heroicons/cog-6-tooth-20-solid';
-    import Check from '~icons/heroicons/check-20-solid';
-    import UserPlus from '~icons/heroicons/user-plus-20-solid';
-    import Search from '~icons/heroicons/magnifying-glass-20-solid';
-    import Trash from '~icons/heroicons/trash-20-solid';
-    import ChevronUpDown from '~icons/heroicons/chevron-up-down-20-solid';
-    import Rows from '~icons/lucide/rows-3';
-    import Columns from '~icons/lucide/columns-3';
-    import GripVertical from '~icons/lucide/grip-vertical';
-    import ChevronUp from '~icons/heroicons/chevron-up-20-solid';
-    import ChevronDown from '~icons/heroicons/chevron-down-20-solid';
-    import CircleDashedOutline from '~icons/ph/circle-dashed';
-    import CircleDashed from '~icons/ph/circle-dashed-fill';
-    import Google from '~icons/custom/google';
-    import CubeOutline from '~icons/ph/cube';
-    import Cube from '~icons/ph/cube-fill';
+    const imports = {
+        home: () => import('~icons/ph/house-bold'),
+        'home-solid': () => import('~icons/ph/house-fill'),
+        users: () => import('~icons/heroicons/user-group-solid'),
+        'users-solid': () => import('~icons/heroicons/user-group'),
+        plus: () => import('~icons/heroicons/plus-20-solid'),
+        'x-mark': () => import('~icons/heroicons/x-mark-20-solid'),
+        'chevron-left': () => import('~icons/heroicons/chevron-left-20-solid'),
+        'chevron-right': () => import('~icons/heroicons/chevron-right-20-solid'),
+        'arrow-up': () => import('~icons/heroicons/arrow-up-20-solid'),
+        'arrow-down': () => import('~icons/heroicons/arrow-down-20-solid'),
+        'arrows-up-down': () => import('~icons/heroicons/arrows-up-down-20-solid'),
+        h1: () => import('~icons/lucide/heading-1'),
+        h2: () => import('~icons/lucide/heading-2'),
+        h3: () => import('~icons/lucide/heading-3'),
+        h4: () => import('~icons/lucide/heading-4'),
+        h5: () => import('~icons/lucide/heading-5'),
+        h6: () => import('~icons/lucide/heading-6'),
+        bold: () => import('~icons/lucide/bold'),
+        italic: () => import('~icons/lucide/italic'),
+        'strike-through': () => import('~icons/lucide/strikethrough'),
+        underline: () => import('~icons/lucide/underline'),
+        quotes: () => import('~icons/lucide/quote'),
+        issues: () => import('~icons/ph/files-fill'),
+        'issues-outline': () => import('~icons/ph/files'),
+        'settings-outline': () => import('~icons/heroicons/cog-6-tooth'),
+        settings: () => import('~icons/heroicons/cog-6-tooth-20-solid'),
+        check: () => import('~icons/heroicons/check-20-solid'),
+        'user-plus': () => import('~icons/heroicons/user-plus-20-solid'),
+        search: () => import('~icons/heroicons/magnifying-glass-20-solid'),
+        trash: () => import('~icons/heroicons/trash-20-solid'),
+        'chevron-up-down': () => import('~icons/heroicons/chevron-up-down-20-solid'),
+        rows: () => import('~icons/lucide/rows-3'),
+        columns: () => import('~icons/lucide/columns-3'),
+        draggable: () => import('~icons/lucide/grip-vertical'),
+        'chevron-up': () => import('~icons/heroicons/chevron-up-20-solid'),
+        'chevron-down': () => import('~icons/heroicons/chevron-down-20-solid'),
+        'circle-dashed-outline': () => import('~icons/ph/circle-dashed'),
+        'circle-dashed': () => import('~icons/ph/circle-dashed-fill'),
+        google: () => import('~icons/custom/google'),
+        'project-outline': () => import('~icons/ph/cube'),
+        project: () => import('~icons/ph/cube-fill')
+    };
 
-    export type IconName = keyof typeof icons;
+    export type IconName = keyof typeof imports;
 
-    const icons = {
-        home: Home,
-        'home-solid': HomeSolid,
-        users: UserGroupOutline,
-        'users-solid': UserGroup,
-        plus: Plus,
-        'x-mark': XMark,
-        'chevron-left': ChevronLeft,
-        'chevron-right': ChevronRight,
-        'arrow-up': ArrowUp,
-        'arrow-down': ArrowDown,
-        'arrows-up-down': ArrowsUpDown,
-        h1: H1,
-        h2: H2,
-        h3: H3,
-        h4: H4,
-        h5: H5,
-        h6: H6,
-        bold: Bold,
-        italic: Italic,
-        'strike-through': StrikeThrough,
-        underline: Underline,
-        quotes: Quotes,
-        issues: Files,
-        'issues-outline': FilesOutline,
-        'settings-outline': SettingsOutline,
-        settings: Settings,
-        check: Check,
-        'user-plus': UserPlus,
-        search: Search,
-        trash: Trash,
-        'chevron-up-down': ChevronUpDown,
-        rows: Rows,
-        columns: Columns,
-        draggable: GripVertical,
-        'chevron-up': ChevronUp,
-        'chevron-down': ChevronDown,
-        'circle-dashed-outline': CircleDashedOutline,
-        'circle-dashed': CircleDashed,
-        google: Google,
-        'project-outline': CubeOutline,
-        project: Cube
-    } as const;
+    const loaded = writable<{
+        [k in keyof typeof imports]?: typeof import('~icons/*').default;
+    }>({});
+
+    const loading: {
+        [k in keyof typeof imports]?: boolean;
+    } = {};
 </script>
 
 <script lang="ts">
-    import type { SvelteHTMLElements, SVGAttributes } from 'svelte/elements';
-    import type { SvelteComponent } from 'svelte';
+    import type { SVGAttributes } from 'svelte/elements';
+    import { writable } from 'svelte/store';
 
     interface Props extends SVGAttributes<SVGElement> {
         name: IconName;
     }
 
     const { name, ...props }: Props = $props();
-    const Icon = $derived<typeof SvelteComponent<SvelteHTMLElements['svg']>>(icons[name]);
+    const Icon = $derived($loaded[name]);
+
+    if (!$loaded[name] && !loading[name]) {
+        loading[name] = true;
+        imports[name]()
+            .then((a) => {
+                $loaded[name] = a.default;
+            })
+            .finally(() => {
+                loading[name] = false;
+            });
+    }
 </script>
 
-<Icon {...props} />
+{#if Icon}
+    <Icon {...props} />
+{/if}
