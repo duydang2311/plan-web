ecf78794f9562d81211f9d34f0cae5e963798642
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-09-22 18:04:02 +0700

    feat: add createAsyncEffect; fix types

--- src/lib/utils/runes.svelte.ts
+++ src/lib/utils/runes.svelte.ts
@@ -1,14 +1,16 @@
 import { untrack } from 'svelte';
 
-export const createEffect = Object.assign(
-    (fn: () => MaybePromise<void | (() => void)>, depsFn?: () => unknown) => {
+export const createEffect: ((fn: () => void | (() => void), depsFn?: () => unknown) => void) & {
+    pre: (fn: () => void | (() => void), depsFn?: () => unknown) => void;
+} = Object.assign(
+    (fn: () => void | (() => void), depsFn?: () => unknown) => {
         $effect(() => {
             depsFn?.();
-            untrack(fn);
+            return untrack(fn);
         });
     },
     {
-        pre: (fn: () => MaybePromise<void | (() => void)>, depsFn?: () => unknown) => {
+        pre: (fn: () => void | (() => void), depsFn?: () => unknown) => {
             $effect.pre(() => {
                 depsFn?.();
                 untrack(fn);
@@ -16,3 +18,20 @@ export const createEffect = Object.assign(
         }
     }
 );
+
+export const createAsyncEffect: (fn: () => MaybePromise<unknown>, depsFn?: () => unknown) => void =
+    Object.assign((fn: () => MaybePromise<unknown>, depsFn?: () => unknown) => {
+        let resolved: false | null = null;
+        $effect(() => {
+            depsFn?.();
+            if (resolved === false) {
+                return;
+            }
+            const ret = untrack(fn) as Promise<void>;
+            if (!(ret instanceof Promise)) {
+                return;
+            }
+            resolved = false;
+            ret.finally(() => (resolved = null));
+        });
+    });
