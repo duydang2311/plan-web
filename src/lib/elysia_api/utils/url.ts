d62bb4bd0a07dc7cd10054118df021918bcd63d1
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-10-13 00:18:02 +0700

    chore: remove hono

--- src/lib/elysia_api/utils/url.ts
+++ src/lib/elysia_api/utils/url.ts
@@ -1,32 +1,3 @@
-import type { Context } from 'hono';
-
-export function queryParams<T extends Record<string, unknown>>(c: Context, defaultValues: T): T {
-    const obj: T = { ...defaultValues };
-    for (const k in obj) {
-        let value: number | string | undefined = c.req.query(k);
-        if (typeof defaultValues[k] === 'boolean') {
-            if (value != null) {
-                obj[k] = (value === 'false' ? false : true) as T[Extract<keyof T, string>];
-            }
-            continue;
-        }
-
-        if (value != null) {
-            if (typeof defaultValues[k] === 'number') {
-                value = Number(value);
-                if (isNaN(value)) {
-                    value = defaultValues[k];
-                }
-            }
-            obj[k] = value as T[Extract<keyof T, string>];
-        } else if (obj[k] == null) {
-            // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-            delete obj[k];
-        }
-    }
-    return obj;
-}
-
 export function queryParamsDict<T extends Record<string, unknown>>(
     dict: Record<string, string | undefined>,
     defaultValues: T
