eafcd32dec2ee9a17730be5acabd569a61747d6c
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-11-24 19:00:13 +0700

    feat: add link to create project page

--- src/routes/(auth)/[path]/projects/+page.svelte
+++ src/routes/(auth)/[path]/projects/+page.svelte
@@ -3,7 +3,7 @@
     import { pipe } from '@baetheus/fun/fn';
     import { createQuery, useQueryClient } from '@tanstack/svelte-query';
     import { DateTime } from 'luxon';
-    import { Link, Pagination, Row, Table, Th, THead } from '~/lib/components';
+    import { Link, Pagination, Row, Table, Th, THead, Button, Input, Icon } from '~/lib/components';
     import { useRuntime } from '~/lib/contexts/runtime.client';
     import { paginatedList, type PaginatedList } from '~/lib/models/paginatedList';
     import { TE } from '~/lib/utils/functional';
@@ -53,7 +53,25 @@
     );
 </script>
 
-<main class="h-full flex flex-col justify-between overflow-auto">
+<main class="h-full grid grid-rows-[auto_1fr] content-start overflow-auto">
+    <div class="flex justify-between border-b border-b-base-border-2 px-4 py-1">
+        <div class="relative">
+            <Input
+                id="search"
+                type="text"
+                class="w-48 border-none focus:ring-0 shadow-none pl-8 py-1"
+                placeholder="Search by name"
+            />
+            <Icon
+                name="search"
+                class="absolute left-0 top-1/2 translate-x-1/2 -translate-y-1/2 text-base-fg-ghost"
+            />
+        </div>
+        <Button as="link" href="/{$page.params['path']}/projects/new" variant="primary" filled={false} size="sm" class="w-fit flex gap-2 items-center">
+            <Icon name="plus" />
+            Create Project
+        </Button>
+    </div>
     <Table style="grid-template-columns: auto 1fr auto auto max-content;">
         <THead>
             <Row class="py-2">
@@ -67,7 +85,7 @@
         <tbody>
             {#if !$query.data || $query.data.items.length === 0}
                 <Row>
-                    <td style="grid-column: 1 / -1;">
+                    <td class="col-span-full">
                         <span class="text-base-fg-ghost"><italic>No projects yet.</italic></span>
                     </td>
                 </Row>
