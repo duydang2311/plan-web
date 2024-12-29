1897cae8b9f7f368f1944cc434af88aa839b6bd9
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-12-29 19:39:50 +0700

    refactor(home): prefetch workspace list

--- src/routes/(auth-optional)/(home)/+page.ts
+++ src/routes/(auth-optional)/(home)/+page.ts
@@ -2,10 +2,16 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ parent, data }) => {
     const { queryClient } = await parent();
-    if (!queryClient.isFetching) {
+
+    if (data.user) {
         await queryClient.prefetchQuery({
             queryKey: ['team-invitations'],
             queryFn: () => data.teamInvitationList
+        });
+
+        await queryClient.prefetchQuery({
+            queryKey: ['workspaces', { userId: data.user.id }],
+            queryFn: () => data.workspaceList
         });
     }
     return data;
