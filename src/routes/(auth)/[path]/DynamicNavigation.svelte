2ce2f1947f5d2d68eb90ae9f59520861fabd72f0
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2025-01-21 13:11:48 +0700

    refactor(DynamicNavigation): remove if scope

--- src/routes/(auth)/[path]/DynamicNavigation.svelte
+++ src/routes/(auth)/[path]/DynamicNavigation.svelte
@@ -3,34 +3,32 @@
     import { tsap } from '~/lib/utils/transition';
     import NavigationItem from './NavigationItem.svelte';
 
-    const routes = $derived<Required<RouteMeta>[] | undefined>(
+    const routes = $derived(
         page.data.routes
             ?.filter((a) => a.breadcrumb === true && a.meta.navigation != null)
-            .map((a) => (a as { meta: Required<RouteMeta> }).meta)
+            .map((a) => (a as { meta: Required<RouteMeta> }).meta) ?? []
     );
 </script>
 
-{#if routes != null && routes.length > 0}
-    {#each routes as { href, navigation } (href)}
-        <div
-            in:tsap|global={(node, gsap) =>
-                gsap.from(node, { opacity: 0, y: '-0.5rem', duration: 0.2, ease: 'power2.out' })}
-            out:tsap|global={(node, gsap) =>
-                gsap.to(node, { opacity: 0, y: '0.5rem', duration: 0.2, ease: 'power2.in' })}
+{#each routes as { href, navigation } (href)}
+    <div
+        in:tsap={(node, gsap) =>
+            gsap.from(node, { opacity: 0, y: '-0.5rem', duration: 0.2, ease: 'power2.out' })}
+        out:tsap={(node, gsap) =>
+            gsap.to(node, { opacity: 0, y: '0.5rem', duration: 0.2, ease: 'power2.in' })}
+    >
+        <h2
+            class="text-sm text-base-fg-5 text-ellipsis whitespace-nowrap overflow-hidden"
+            title={navigation.label}
         >
-            <h2
-                class="text-sm text-base-fg-5 text-ellipsis whitespace-nowrap overflow-hidden"
-                title={navigation.label}
-            >
-                {navigation.label}
-            </h2>
-            <ul class="mt-1 font-medium group text-sm">
-                {#each navigation.entries as { label, href, icon, activeIcon } (href)}
-                    <li>
-                        <NavigationItem {href} {icon} {activeIcon} {label} />
-                    </li>
-                {/each}
-            </ul>
-        </div>
-    {/each}
-{/if}
+            {navigation.label}
+        </h2>
+        <ul class="mt-1 font-medium group text-sm">
+            {#each navigation.entries as { label, href, icon, activeIcon } (href)}
+                <li>
+                    <NavigationItem {href} {icon} {activeIcon} {label} />
+                </li>
+            {/each}
+        </ul>
+    </div>
+{/each}
