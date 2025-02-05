5577effd3f8759e44af955f30cf95b6b4548a4a5
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2025-02-05 23:05:36 +0700

    fix: fix runtime queryClient

--- src/lib/contexts/runtime.client.ts
+++ src/lib/contexts/runtime.client.ts
@@ -1,17 +1,10 @@
-import { env } from '$env/dynamic/public';
-import {
-    PUBLIC_REALTIME_PASSWORD,
-    PUBLIC_REALTIME_SERVER,
-    PUBLIC_REALTIME_USERNAME
-} from '$env/static/public';
 import { Cloudinary } from '@cloudinary/url-gen';
+import { type QueryClient } from '@tanstack/svelte-query';
 import defineLazyProperty from 'define-lazy-prop';
 import type { Context } from 'effect';
 import { getContext, setContext } from 'svelte';
 import type { HttpClient } from '../services/http_client';
-import { NATSRealtime, type Realtime } from '../services/realtime.client';
-import { UniversalHttpClient } from '../services/universal_http_client';
-import { type QueryClient, useQueryClient } from '@tanstack/svelte-query';
+import { type Realtime } from '../services/realtime.client';
 
 interface Runtime {
     readonly api: Context.Tag.Service<HttpClient>;
@@ -20,41 +13,12 @@ interface Runtime {
     readonly queryClient: QueryClient;
 }
 
-export function setRuntime() {
-    const runtime = {};
-    defineLazyProperty(
-        runtime,
-        'realtime',
-        () =>
-            new NATSRealtime({
-                servers: PUBLIC_REALTIME_SERVER,
-                username: PUBLIC_REALTIME_USERNAME,
-                password: PUBLIC_REALTIME_PASSWORD
-            })
-    );
-    defineLazyProperty(
-        runtime,
-        'api',
-        () =>
-            new UniversalHttpClient({
-                baseUrl: `${globalThis.origin}/api`,
-                version: 'v1',
-                fetch: globalThis.fetch
-            })
-    );
-    defineLazyProperty(
-        runtime,
-        'cloudinary',
-        () =>
-            new Cloudinary({
-                cloud: {
-                    cloudName: env.PUBLIC_CLOUDINARY_CLOUD_NAME
-                },
-                url: { secure: true }
-            })
-    );
-    defineLazyProperty(runtime, 'queryClient', () => useQueryClient());
-    return setContext('runtime', runtime as Runtime);
+export function setRuntime(value: Record<keyof Runtime, () => Runtime[keyof Runtime]>) {
+    const lazy = {};
+    for (const k in value) {
+        defineLazyProperty(lazy, k, value[k as keyof typeof value]);
+    }
+    return setContext('runtime', lazy as Runtime);
 }
 
 export function useRuntime() {
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,39 @@
 <script lang="ts">
+    import { env } from '$env/dynamic/public';
+    import { Cloudinary } from '@cloudinary/url-gen/index';
     import { QueryClientProvider } from '@tanstack/svelte-query';
     import { onMount, type Snippet } from 'svelte';
     import Toaster from '~/lib/components/Toaster.svelte';
     import { setRuntime } from '~/lib/contexts/runtime.client';
+    import { NATSRealtime } from '~/lib/services/realtime.client';
+    import { UniversalHttpClient } from '~/lib/services/universal_http_client';
     import type { LayoutData } from './$types';
     import './+layout.css';
+    import { page } from '$app/state';
 
     const { data, children }: { data: LayoutData; children: Snippet } = $props();
-    const runtime = setRuntime();
+    const runtime = setRuntime({
+        realtime: () =>
+            new NATSRealtime({
+                servers: env.PUBLIC_REALTIME_SERVER,
+                username: env.PUBLIC_REALTIME_USERNAME,
+                password: env.PUBLIC_REALTIME_PASSWORD
+            }),
+        api: () =>
+            new UniversalHttpClient({
+                baseUrl: `${page.url.origin}/api`,
+                version: 'v1',
+                fetch: globalThis.fetch
+            }),
+        cloudinary: () =>
+            new Cloudinary({
+                cloud: {
+                    cloudName: env.PUBLIC_CLOUDINARY_CLOUD_NAME
+                },
+                url: { secure: true }
+            }),
+        queryClient: () => data.queryClient
+    });
 
     function onColorSchemePreferenceChange(e: MediaQueryListEvent) {
         if (e.matches) {
