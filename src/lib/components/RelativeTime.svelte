7ae1774795b4e6af0e9c20b962e068abe7cad27f
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2025-02-19 00:21:59 +0700

    refactor(RelativeTime): use a global interval to update time

--- src/lib/components/RelativeTime.svelte
+++ src/lib/components/RelativeTime.svelte
@@ -1,20 +1,54 @@
+<script lang="ts" module>
+    let interval = 0;
+    const refs: Ref<{ time: DateTime; format: string }>[] = [];
+    export const add = (ref: Ref<{ time: DateTime; format: string }>) => {
+        refs.push(ref);
+        if (interval === 0) {
+            interval = setInterval(update, 1000) as unknown as number;
+        }
+    };
+    export const remove = (ref: Ref<{ time: DateTime; format: string }>) => {
+        const index = refs.findIndex((a) => a === ref);
+        if (index !== -1) {
+            refs.splice(index, 1);
+        }
+        if (refs.length === 0) {
+            clearInterval(interval);
+            interval = 0;
+        }
+    };
+    export const update = () => {
+        for (const ref of refs) {
+            if (ref.value) {
+                ref.value = {
+                    ...ref.value,
+                    format: ref.value.time.toRelative() ?? 'N/A'
+                };
+            }
+        }
+    };
+</script>
+
 <script lang="ts">
     import { DateTime, type ToRelativeOptions } from 'luxon';
+    import { createRef, type Ref } from '../utils/runes.svelte';
     import { onMount } from 'svelte';
 
-    const { time, options }: { time: string | DateTime; options?: ToRelativeOptions } = $props();
-    let format = $state.raw(
-        (typeof time === 'string' ? DateTime.fromISO(time) : time).toRelative(options)
-    );
+    let { time, options }: { time: string | DateTime; options?: ToRelativeOptions } = $props();
+    if (typeof time === 'string') {
+        time = DateTime.fromISO(time);
+    }
+    const ref = createRef({
+        time,
+        format: time.toRelative(options) ?? 'N/A'
+    });
 
     onMount(() => {
-        const interval = setInterval(() => {
-            format = (typeof time === 'string' ? DateTime.fromISO(time) : time).toRelative(options);
-        }, 1000);
+        add(ref);
         return () => {
-            clearInterval(interval);
+            remove(ref);
         };
     });
 </script>
 
-{format}
+{ref.value.format}
