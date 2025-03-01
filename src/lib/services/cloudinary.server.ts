51e69abab4b394073403c2095a173cb708d9d2c4
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2025-03-01 14:57:25 +0700

    fix(cloudinary): configure cloudinary

--- src/lib/services/cloudinary.server.ts
+++ src/lib/services/cloudinary.server.ts
@@ -1,6 +1,16 @@
+import { env as privateEnv } from '$env/dynamic/private';
+import { env as publicEnv } from '$env/dynamic/public';
 import { v2 } from 'cloudinary';
 import { Context, Layer } from 'effect';
 
 export class Cloudinary extends Context.Tag('@app/Cloudinary')<Cloudinary, typeof v2>() {
-    public static readonly Live = Layer.sync(Cloudinary, () => v2);
+    public static readonly Live = Layer.sync(Cloudinary, () => {
+        v2.config({
+            cloud_name: publicEnv.PUBLIC_CLOUDINARY_CLOUD_NAME,
+            api_key: privateEnv.CLOUDINARY_API_KEY,
+            api_secret: privateEnv.CLOUDINARY_API_SECRET,
+            secure: true
+        });
+        return v2;
+    });
 }
