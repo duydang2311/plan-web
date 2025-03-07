93cc619c1f375be672ad37f3c4c123dd4d9ec8ff
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2025-03-08 00:21:55 +0700

    refactor(+error): improve error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,10 +1,39 @@
 <script lang="ts">
     import { page } from '$app/state';
+    import { Button } from '~/lib/components';
+    import { IconBack } from '~/lib/components/icons';
 </script>
 
-<main class="content-center text-center min-w-screen min-h-screen w-full h-full">
+<svelte:head>
+    <title>Error - {page.status} ({page.error?.code})</title>
+</svelte:head>
+
+<main class="min-h-screen content-center">
     {#if page.error}
-        <h1>{page.status}</h1>
-        <p>{page.error.message} ({page.error.code})</p>
+        <div class="max-w-paragraph-lg mx-auto text-center">
+            <h1 class="text-9xl font-black">{page.status}</h1>
+            <p class="text-base-fg-ghost">Oops, something went wrong!</p>
+            <p class="mt-4">
+                Error details: "{page.error.message}"
+                {#if page.error.code}
+                    (<strong>{page.error.code}</strong>).
+                {/if}
+            </p>
+            <pre class="text-base-fg-5 mx-auto mt-4 w-fit text-left">{JSON.stringify(
+                    page.error,
+                    null,
+                    4
+                )}
+            </pre>
+            <Button
+                as="link"
+                href="/"
+                variant="primary"
+                class="mx-auto mt-16 flex w-fit items-center gap-2"
+            >
+                <IconBack />
+                Back to home
+            </Button>
+        </div>
     {/if}
 </main>
