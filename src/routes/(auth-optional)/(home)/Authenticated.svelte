f0469922ca06f9f893239a997fda46b62736917e
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2025-06-16 17:58:56 +0700

    fix: add missing Create Workspace button

--- src/routes/(auth-optional)/(home)/Authenticated.svelte
+++ src/routes/(auth-optional)/(home)/Authenticated.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-    import { Avatar } from '~/lib/components';
+    import { Avatar, Button } from '~/lib/components';
     import LandingLayout from '~/lib/components/layouts/LandingLayout.svelte';
     import TeamInvitations from './TeamInvitations.svelte';
     import WorkspaceList from './WorkspaceList.svelte';
     import { createUserQuery } from './utils';
     import NotificationBell from '~/routes/(auth)/[path]/NotificationBell.svelte';
+    import { IconPlus } from '~/lib/components/icons';
 
     const { userId }: { userId: string } = $props();
     const query = createUserQuery(() => ({ userId }));
@@ -24,12 +25,25 @@
     <main class="container mx-auto p-4">
         <TeamInvitations {userId} />
         <section>
-            <h1 class="font-h-bold capitalize">Your workspaces</h1>
-            <div class="mb-4">
-                <p class="c-text-secondary">
-                    Workspace is where you organize projects, teams, and issues. Start by clicking
-                    on a workspace to manage them or creating a new one.
-                </p>
+            <div class="mb-4 flex items-baseline justify-between gap-8">
+                <div>
+                    <h1 class="font-h-bold capitalize">Your workspaces</h1>
+                    <div>
+                        <p class="c-text-secondary">
+                            Workspace is where you organize projects, teams, and issues. Start by
+                            clicking on a workspace to manage them or creating a new one.
+                        </p>
+                    </div>
+                </div>
+                <Button
+                    as="link"
+                    href="/new"
+                    variant="primary"
+                    class="flex w-fit items-center gap-2 capitalize"
+                >
+                    <IconPlus />
+                    <span>Create workspace</span>
+                </Button>
             </div>
             <WorkspaceList {userId} />
         </section>
